c2940d32a3c3d627c5c4dbdc15fe7bbd27a24e53
Syendal1 <snehith.yendal0313@gmail.com>
2016-08-14 21:41:36 -0700

    Initial Commit

---
+++ Pokedex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by SnehithNitin on 8/14/16.
+//  Copyright © 2016 Snehith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pokedex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pokedex
+//
+//  Created by SnehithNitin on 8/14/16.
+//  Copyright © 2016 Snehith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
